d3e148f7610b20aa1bea92444f1535e1edc3009c
eron97 <eronsantosbetine@gmail.com>
2024-01-07 11:02:57 -0300

    create tags json request

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,9 +1,26 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/eron97/api-mvc.git/src/config/error_messages"
+	"github.com/eron97/api-mvc.git/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
 
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		errorMessage := error_messages.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()),
+		)
+
+		c.JSON(errorMessage.Code, errorMessage)
+		return
+	}
+
+	c.JSON(http.StatusOK, userRequest)
 }
