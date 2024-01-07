2178f49084e9112e7dd1198ca08c420e94f071bb
eron97 <eronsantosbetine@gmail.com>
2024-01-07 10:31:12 -0300

    route create_user request response

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+
+	"github.com/eron97/api-mvc.git/src/controller/routes"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 
 }
--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -1,13 +1,16 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/eron97/api-mvc.git/src/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/getUserById/:userId", func(c *gin.Context) {})
-	r.GET("/getUserByEmail/:userEmail", func(c *gin.Context) {})
-	r.POST("/createUser", func(c *gin.Context) {})
-	r.PUT("/updateUser/:userId", func(c *gin.Context) {}, func(c *gin.Context) {})
-	r.DELETE("/deleteUser/:userId", func(c *gin.Context) {})
+	r.GET("/getUserById/:userId", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 }
 
 /*
