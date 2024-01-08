006d48a25ea2ca37903a367656344b7402d1dcc5
eron97 <eronsantosbetine@gmail.com>
2024-01-08 07:52:57 -0300

    logs uber

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,26 +1,58 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/eron97/api-mvc.git/src/config/error_messages"
 	"github.com/eron97/api-mvc.git/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translation "github.com/go-playground/validator/v10/translations/en"
 )
 
-func CreateUser(c *gin.Context) {
+var (
+	Validate = validator.New()
+	transl   ut.Translator
+)
 
+func init() {
+	if val, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		en := en.New()
+		unt := ut.New(en, en)
+		transl, _ = unt.GetTranslator("en")
+		en_translation.RegisterDefaultTranslations(val, transl)
+	}
+}
+
+func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		errorMessage := error_messages.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()),
-		)
 
-		c.JSON(errorMessage.Code, errorMessage)
-		return
+		if ve, ok := err.(validator.ValidationErrors); ok {
+			errorMessage := error_messages.NewBadRequestError("Erro de validação")
+
+			for _, fieldError := range ve {
+				cause := error_messages.CausesMessages{
+					Field:   fieldError.Field(),
+					Message: fieldError.Translate(transl),
+				}
+				errorMessage.Causes = append(errorMessage.Causes, cause)
+			}
+
+			c.JSON(http.StatusBadRequest, gin.H{"Erro de validação": errorMessage})
+			return
+		}
+
+		if err := c.ShouldBindJSON(&userRequest); err != nil {
+			errorMessage := error_messages.NewUnmarshalError("Campos preenchidos não correspondem ao seus tipos atribuídos")
+			c.JSON(http.StatusBadRequest, errorMessage)
+			return
+		}
 	}
 
-	c.JSON(http.StatusOK, "Usuário criado com sucesso!")
+	c.JSON(http.StatusOK, gin.H{"message": "Usuário criado com sucesso", "user": userRequest})
 }
